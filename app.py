cb2969d761d19dc3e1978db05f5bf6d43c28254b
Felice Forby <morinoko@hey.com>
2026-04-02 08:44:01 -0400

    Add db_create route

--- app.py
+++ app.py
@@ -7,8 +7,26 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Felice in 3308'
 
-@app.route('/db-test')
+@app.route('/db_test')
 def db_test():
     conn = psycopg2.connect("postgresql://cspb_example_db_user:BDrT7caz1cGMsEP0Kql2ofs0Dot7krOk@dpg-d7763kdm5p6s739fs2c0-a/cspb_example_db")
     conn.close()
     return 'Database connection successful!'
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgresql://cspb_example_db_user:BDrT7caz1cGMsEP0Kql2ofs0Dot7krOk@dpg-d7763kdm5p6s739fs2c0-a/cspb_example_db")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
+    return 'Table created successfully!'
